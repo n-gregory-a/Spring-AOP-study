5f1a39b4401deb47468e8ad7c00b43a561e5d2ad
Grigory <ovonely@gmail.com>
2019-12-12 21:40:05 +0700

    initial creating of starting classes

---
+++ src/main/java/gn/aopdemo/MainDemoApp.java
@@ -0,0 +1,23 @@
+package gn.aopdemo;
+
+import gn.aopdemo.dao.AccountDAO;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class MainDemoApp {
+
+    public static void main(String[] args) {
+
+        // read spring config java class
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(DemoConfig.class);
+
+        // get the bean from spring container
+        AccountDAO accountDAO = context.getBean("accountDAO", AccountDAO.class);
+
+        // call the business method
+        accountDAO.addAccount();
+
+        // close the context
+        context.close();
+    }
+
+}
