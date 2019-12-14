674b96b20e3293723c20127f6ab881a58c275b78
Grigory <ovonely@gmail.com>
2019-12-14 13:58:02 +0700

    changed Aspect to match only AccountDAO.addAccount() method

--- src/main/java/gn/aopdemo/dao/AccountDAO.java
+++ src/main/java/gn/aopdemo/dao/AccountDAO.java
@@ -5,6 +5,6 @@ import org.springframework.stereotype.Component;
 @Component
 public class AccountDAO {
     public void addAccount() {
-        System.out.printf(getClass() +": DOING MY DB WORK: ADDING AN ACCOUNT");
+        System.out.println(getClass() +": DOING MY DB WORK: ADDING AN ACCOUNT");
     }
 }
