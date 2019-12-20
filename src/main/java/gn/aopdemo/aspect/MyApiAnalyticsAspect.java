abb2d1d9a6dc4f49a6ea75ce46cc76677954f8cf
Grigory <ovonely@gmail.com>
2019-12-20 23:16:17 +0700

    AOP: ordering aspects - create AopExpressions class to contain all pointcuts - create different classes for each aspect - order aspects by @Order annotation

--- src/main/java/gn/aopdemo/aspect/MyApiAnalyticsAspect.java
+++ src/main/java/gn/aopdemo/aspect/MyApiAnalyticsAspect.java
@@ -7,12 +7,13 @@ import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
-@Order(1)
-public class MyDemoLoggingAspect {
+@Order(2)
+public class MyApiAnalyticsAspect {
 
     @Before("gn.aopdemo.aspect.AopExpressions.forDaoNoGetterSetter()")
-    public void beforeAddAccountAdvice() {
-        System.out.println("\n=======>>> Execution @Before advice on addAccount()");
+    public void performApiAnalytics() {
+        System.out.println("\n=====>>> Perform API analytics");
     }
+
 
 }
--- src/main/java/gn/aopdemo/aspect/MyCloudLogAsyncAspect.java
+++ src/main/java/gn/aopdemo/aspect/MyCloudLogAsyncAspect.java
@@ -7,12 +7,13 @@ import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
-@Order(1)
-public class MyDemoLoggingAspect {
+@Order(3)
+public class MyCloudLogAsyncAspect {
 
     @Before("gn.aopdemo.aspect.AopExpressions.forDaoNoGetterSetter()")
-    public void beforeAddAccountAdvice() {
-        System.out.println("\n=======>>> Execution @Before advice on addAccount()");
+    public void loadToCloudAsync() {
+        System.out.println("\n=======>>> Logging to Cloud in async fashion");
     }
+
 
 }
