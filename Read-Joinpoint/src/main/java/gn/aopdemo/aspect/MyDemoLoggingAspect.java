e1831280f654a020dec02bd05c308f43285cd6cb
Grigory <ovonely@gmail.com>
2019-12-21 18:16:06 +0700

    AOP: joinpoints - reading method signature

--- Read-Joinpoint/src/main/java/gn/aopdemo/aspect/MyDemoLoggingAspect.java
+++ Read-Joinpoint/src/main/java/gn/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -1,7 +1,9 @@
 package gn.aopdemo.aspect;
 
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.reflect.MethodSignature;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
@@ -11,8 +13,16 @@ import org.springframework.stereotype.Component;
 public class MyDemoLoggingAspect {
 
     @Before("gn.aopdemo.aspect.AopExpressions.forDaoNoGetterSetter()")
-    public void beforeAddAccountAdvice() {
+    public void beforeAddAccountAdvice(JoinPoint joinPoint) {
         System.out.println("\n=======>>> Execution @Before advice on addAccount()");
+
+        // display the method signature
+        MethodSignature methodSignature = (MethodSignature) joinPoint.getSignature();
+
+        System.out.println("Method: " + methodSignature);
+
+        // display method arguments
+
     }
 
 }
