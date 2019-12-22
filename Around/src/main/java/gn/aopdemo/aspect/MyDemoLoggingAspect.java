cc189d43ca495477bb5c13a1ad868d0b6120a295
Grigory <ovonely@gmail.com>
2019-12-22 20:06:08 +0700

    AOP: @Around:  - creating Around advise

--- Around/src/main/java/gn/aopdemo/aspect/MyDemoLoggingAspect.java
+++ Around/src/main/java/gn/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -2,6 +2,7 @@ package gn.aopdemo.aspect;
 
 import gn.aopdemo.Account;
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.*;
 import org.aspectj.lang.reflect.MethodSignature;
 import org.springframework.core.annotation.Order;
@@ -13,6 +14,29 @@ import java.util.List;
 @Component
 @Order(1)
 public class MyDemoLoggingAspect {
+
+    @Around("execution(* gn.aopdemo.service.*.getFortune(..))")
+    public Object aroundGetFortune(ProceedingJoinPoint proceedingJoinPoint) throws Throwable{
+
+        // print out method we are advising on
+        String method = proceedingJoinPoint.getSignature().toShortString();
+        System.out.println("\n=====>>>> Executing @Around on method: " + method);
+
+        // get begin timestamp
+        long begin = System.currentTimeMillis();
+
+        // now, let's execute the method
+        Object result = proceedingJoinPoint.proceed();
+
+        // get end timestamp
+        long end = System.currentTimeMillis();
+
+        // compute duration and display it
+        long duration = end - begin;
+        System.out.println("\n======>>> Duration: " + duration/1000 + " seconds");
+
+        return result;
+    }
 
     @After(value = "execution(* gn.aopdemo.dao.AccountDAO.findAccounts(..))")
     public void afterFinallyAccountsAdvice(JoinPoint joinPoint) {
